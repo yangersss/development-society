a13fd79734fff275167bab7bc19ac75ba7e4b247
yangersss <edwardyang609@gmail.com>
2023-10-27 13:08:15 -0400

    just a little chatgpt test

---
+++ personal stuff/script.js
@@ -0,0 +1,7 @@
+document.getElementById("logo").addEventListener("mouseover", function() {
+    this.style.transform = "scale(1.1)";
+});
+
+document.getElementById("logo").addEventListener("mouseout", function() {
+    this.style.transform = "scale(1)";
+});
